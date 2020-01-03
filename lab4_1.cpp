103a33d97e6a2468923f34471003dc43268e49b3
ultramanbabe <ultraman_babeclub@hotmail.com>
2020-01-03 15:00:06 +0700

    1

--- lab4_1.cpp
+++ lab4_1.cpp
@@ -1,1 +1,17 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
 
+double cal(int x){
+    double e =  2.71828;
+    double y = (3*(pow(x,3)))+(2*e)+(pow(2,(2*x+1)))+(sqrt(pow(x,2)+1));
+    return y;
+}
+
+int main(){
+    int x;
+    cout<<"Enter x = ";
+    cin>>x;
+    cout<<"Result y = "<<cal(x);
+    return 0;
+}
