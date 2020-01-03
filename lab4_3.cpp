7138576ef02b5245af24b74e0a183a6e9d738ea4
ultramanbabe <ultraman_babeclub@hotmail.com>
2020-01-03 15:55:25 +0700

    1

--- lab4_3.cpp
+++ lab4_3.cpp
@@ -1,1 +1,21 @@
+#include<iostream>
+using namespace std;
 
+int findDivisor(int x){
+    for(int i = 2; i < x ; i++){
+        if(x%i==0){
+            return i;
+        }else{
+            ;
+        }
+    }
+}
+
+int main(){
+    int x;
+    cout<<"Find Divisor : Please Input : ";
+    cin>>x;
+    cout<<"Output = "<<findDivisor(x);
+
+    return 0;
+}
